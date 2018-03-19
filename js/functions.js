53ec57772c58a52fa3561f3096d4746e06bd940c
christianmenk <christianmenk@gmail.com>
2018-03-19 16:32:54 -0400

    Code cleanup
    This commit includes:
    Comments
    Some UI updates
    No more alerts used for testing

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
 function parseInput(text){
-    alert(text);
+    // This function will be used for parsing input and rejecting or accepting it
+    // Imports from the file uploaded
+    $('#outputTA').val(text);
+
+    // Scroll to output
+    $('html, body').animate({
+        scrollTop: $("#output").offset().top
+    }, 1000);
 }
