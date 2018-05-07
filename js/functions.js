baf03be3960f054c970b3ccd8797facd2f8a3b01
christianmenk <christianmenk@gmail.com>
2018-05-07 12:49:25 -0400

    Alphabet is now learned through test file

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,20 @@
-function parseInput(text){
-    // This function will be used for parsing input and rejecting or accepting it
-    // Imports from the file uploaded
-    $('#outputTA').val(text);
-
+function run(text){
     // Scroll to output
     $('html, body').animate({
         scrollTop: $("#output").offset().top
     }, 1000);
+
+    learnAlphabet(text);
+}
+
+function learnAlphabet(text){
+    // This just removed the newlines from the input file so they aren't rendered as part of the alphabet
+    text = text.replace(/\r?\n?/g, '');
+    var alphabet = [];
+    for(var i = 0; i < text.length; i++){
+        if((!alphabet.includes(text.charAt(i))) && (text.charAt(i) !== "\n"))
+        alphabet.push(text.charAt(i));
+    }
+
+    $('#acceptedTA').val("Alphabet from test file: " + alphabet);
 }
